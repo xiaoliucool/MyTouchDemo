41e10ea6afb387d31eff15c5f610b77c17539bb0
刘文超 <s_liuwc@jiedaibao.com>
2016-08-02 11:34:57 +0800

    测试

--- app/src/main/java/com/xiaoliu/mytouchdemo/MainActivity.java
+++ app/src/main/java/com/xiaoliu/mytouchdemo/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements MyDoubleClickCall
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initView();
     }
 
